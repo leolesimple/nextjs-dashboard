3d259fd9f48e84010a5f665fb8992ccc14bfcc09
Léo Lesimple <leolesimple@icloud.com>
2025-06-27 13:54:23 +0200

    feat: update login prompt to include placeholder credentials for user guidance

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -24,7 +24,7 @@ export default function LoginForm() {
       <form action={formAction} className="space-y-3">
         <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
           <h1 className={`${lusitana.className} mb-3 text-2xl`}>
-            Please log in to continue.
+            Please log in to continue. <br/>
             <span className="text-sm font-normal text-gray-500">
                 {'login : user@nextmail.com | password : 123456'}
             </span>
